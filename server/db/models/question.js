b2019bd7d67d7955bd80dc619fb34c7519b1a482
Alexandra Ash <alexandraleah@gmail.com>
2018-08-10 13:14:22 -0400

    feature(model): created question model

--- server/db/models/question.js
+++ server/db/models/question.js
@@ -1,2 +1,15 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+
+const Question = db.define('questionS', {
+  theQuestion: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {notEmpty: true}
+  },
+  category: {
+    type: Sequelize.ENUM('history', 'art', 'geography')
+  }
+})
+
+module.exports = Question
