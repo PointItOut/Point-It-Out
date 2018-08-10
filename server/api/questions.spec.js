6db8e455a3d074f7e9365995c8b43c664f234199
Alexandra Ash <alexandraleah@gmail.com>
2018-08-10 18:40:59 -0400

    testing(routes) writing question tests

--- server/api/questions.spec.js
+++ server/api/questions.spec.js
@@ -14,30 +14,57 @@ describe('Question routes', () => {
       {
         theQuestion: 'What is the capital of Russia',
         category: 'geography'
+      },
+      {
+        theQuestion: 'Who was the first U.S. President',
+        category: 'history'
+      },
+      {
+        theQuestion: 'Who was the artist who painted Starry Night',
+        category: 'art'
       }
-      // {
-      //   theQuestion: 'Who was the first U.S. President',
-      //   category: 'history'
-      // },
-      // {
-      //   theQuestion: 'Who was the artist who painted Starry Night',
-      //   category: 'art'
-      // }
     ]
-    beforeEach(() => {
-      return Question.bulkCreate(questions, {returning: true})
+
+    const choices = [
+      {
+        theChoice: 'Moscow',
+        isCorrect: true,
+        isPicture: false
+      },
+      {
+        theChoice: 'Boston',
+        isCorrect: false,
+        isPicture: false
+      },
+      {
+        theChoice: 'Tel Aviv',
+        isCorrect: false,
+        isPicture: false
+      },
+      {
+        theChoice: 'D.C.',
+        isCorrect: false,
+        isPicture: false
+      }
+    ]
+
+    beforeEach(async () => {
+      Question.bulkCreate(questions)
+      Choice.bulkCreate(choices)
+      const myQuestion = await Question.findOne()
+      myQuestion.setChoices(choices)
     })
     it('GET /api/questions', async () => {
       const res = await request(app)
         .get('/api/questions')
         .expect(200)
       expect(res.body).to.be.an('array')
-      expect(res.body.length).to.be.equal(1)
+      expect(res.body.length).to.be.equal(3)
       expect(res.body[0].theQuestion).to.be.equal(
         'What is the capital of Russia'
       )
       console.log('res.body is', res.body)
-      // expect(res.body[0].choices).to.be.equal(4)
+      expect(res.body[0].choices).to.be.equal(4)
     })
   })
 })
