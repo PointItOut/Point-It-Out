ac475fcb48d4dee14412bfd74ea1981d90e019f8
Alexandra Ash <alexandraleah@gmail.com>
2018-08-10 16:15:05 -0400

    tests(routes): tests for question routes

--- server/api/questions.spec.js
+++ server/api/questions.spec.js
@@ -14,15 +14,15 @@ describe('Question routes', () => {
       {
         theQuestion: 'What is the capital of Russia',
         category: 'geography'
-      },
-      {
-        theQuestion: 'Who was the first U.S. President',
-        category: 'history'
-      },
-      {
-        theQuestion: 'Who was the artist who painted Starry Night',
-        category: 'art'
       }
+      // {
+      //   theQuestion: 'Who was the first U.S. President',
+      //   category: 'history'
+      // },
+      // {
+      //   theQuestion: 'Who was the artist who painted Starry Night',
+      //   category: 'art'
+      // }
     ]
     beforeEach(() => {
       return Question.bulkCreate(questions, {returning: true})
@@ -32,7 +32,12 @@ describe('Question routes', () => {
         .get('/api/questions')
         .expect(200)
       expect(res.body).to.be.an('array')
-      expect(res.body.length).to.be.equal(3)
+      expect(res.body.length).to.be.equal(1)
+      expect(res.body[0].theQuestion).to.be.equal(
+        'What is the capital of Russia'
+      )
+      console.log('res.body is', res.body)
+      // expect(res.body[0].choices).to.be.equal(4)
     })
   })
 })
