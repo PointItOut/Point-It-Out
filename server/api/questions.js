b0122fe0422dbbf6a2072591e39500b21c331ad9
Kate Dubitski <kate.dubitski@gmail.com>
2018-08-13 19:30:55 -0400

    randomized questions

--- server/api/questions.js
+++ server/api/questions.js
@@ -6,6 +6,7 @@ router.get('/', async (req, res, next) => {
     const questions = await Question.findAll({
       include: {model: Choice}
     })
+
     res.json(questions)
   } catch (err) {
     next(err)
@@ -19,10 +20,24 @@ router.get('/:category', async (req, res, next) => {
       where: {category: category},
       include: {model: Choice}
     })
-    res.json(questions)
+    let questionArrey = [...questions]
+    questionArrey.map(
+      question => (question.choices = shuffleArrey(question.choices))
+    )
+    res.json(shuffleArrey(questionArrey))
+    // res.json(questions)
   } catch (err) {
     next(err)
   }
 })
+
+function shuffleArrey(array) {
+  let modified = [...array]
+  for (let i = modified.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[modified[i], modified[j]] = [modified[j], modified[i]]
+  }
+  return modified
+}
 
 module.exports = router
