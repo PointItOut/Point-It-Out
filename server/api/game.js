3599dfd5a9c14dbffc7d5663e2b8c3ee2cf5bbd1
Alexandra Ash <alexandraleah@gmail.com>
2018-08-14 11:26:11 -0400

    Update game.js

--- server/api/game.js
+++ server/api/game.js
@@ -23,10 +23,6 @@ router.get('/:name', async (req, res, next) => {
         })
         let token = opentok.generateToken(existgame.sessionId)
 
-        //send the user back as well, although we will still have to update it on state. Or I suppose we could just send the token? seems sort of hacky but it could work. in fact we may not need to store the token at all if we don't care if they can rejoin the game or not.
-        // (if it's the user themselves does it matter if we send the entire object? check what is already on state)
-        //why is it not working only when there are not games on state? I can see why joining an existing game would work but why is the token getting placed on state when there is a game but not when there is not one. weird.
-
         await User.update({ gameId: existgame.id, token }, { where: { id: req.user.id } })
 
         res.json({ token })
