3f175f0668474f722d845209f4a70950229cd25b
Alexandra Ash <alexandraleah@gmail.com>
2018-08-15 13:45:19 -0400

    navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,23 +6,56 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="navbar-brand">
+        <Link to="/">Point It Out</Link>
+      </div>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">Home</li>
+          {/* {isLoggedIn ? (
+            <span>
+              <li className="nav-item">
+                {/* The navbar will show these links after you log in */}
+          {/* <Link to="/home" className="nav-link"> */}
+          {/* Home */}
+          {/* </Link> */}
+          {/* </li>
+              <li className="nav-item">
+                <a href="#" onClick={handleClick} className="nav-link">
+                  Logout
+                </a>
+              </li>
+            </span> */}
+          {/* ) : ( */}
+          {/* <span>
+              {/* The navbar will show these links before you log in */}
+          {/* <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/signup" className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </span>
+          )} */}
+        </ul>
+      </div>
     </nav>
     <hr />
   </div>
