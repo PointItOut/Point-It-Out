802372db82217bc56c9f2bc95ca8a63da2011a2c
Alexandra Ash <alexandraleah@gmail.com>
2018-08-17 11:58:25 -0400

    jumbotron

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -6,35 +6,37 @@ import {connect} from 'react-redux'
 const Splash = ({isLoggedIn}) => {
   return (
     <div className="container">
-      <h1>Point It Out</h1>
-      <h2>A fast paced trivia game for the whole family!</h2>
+      <div className="jumbotron">
+        <h1>Point It Out</h1>
+        <h2>A fast paced trivia game for the whole family!</h2>
 
-      {/* if they are signed in show a play button
+        {/* if they are signed in show a play button
       otherwise display a signup or login button */}
-      {isLoggedIn ? (
-        <button type="button" className="btn btn-info">
-          <Link to="home">Play</Link>
+        {isLoggedIn ? (
+          <button type="button" className="btn btn-info">
+            <Link to="home">Play</Link>
+          </button>
+        ) : (
+          <span>
+            <button type="button" className="btn btn-info">
+              <Link to="/signup">Signup</Link>
+            </button>
+            <button type="button" className="btn btn-info">
+              <Link to="/login">Login</Link>
+            </button>
+          </span>
+        )}
+        {/* button to open modal with instructions */}
+        <button
+          type="button"
+          className="btn btn-info"
+          data-toggle="modal"
+          data-target="#instructionModal"
+        >
+          How to Play
         </button>
-      ) : (
-        <span>
-          <button type="button" className="btn btn-info">
-            <Link to="/signup">Signup</Link>
-          </button>
-          <button type="button" className="btn btn-info">
-            <Link to="/login">Login</Link>
-          </button>
-        </span>
-      )}
-      {/* button to open modal with instructions */}
-      <button
-        type="button"
-        className="btn btn-info"
-        data-toggle="modal"
-        data-target="#instructionModal"
-      >
-        How to Play
-      </button>
-      <Instructions />
+        <Instructions />
+      </div>
     </div>
   )
 }
