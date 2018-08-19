8f48b87af25abc291d881e3ea58aec72e637d00e
Alexandra Ash <alexandraleah@gmail.com>
2018-08-19 13:58:34 -0400

    Update Footer.js

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => (
   <footer className="container-fluid">
     <div className="row">
       <div className="col-sm-12 col-md-10">
-        Created by Alexandra Ash, Amy Berg, Kate Dubitski Kopitchinsli and Sivan
+        Created by Alexandra Ash, Amy Berg, Kate Dubitski Kopitchinski and Sivan
         Gilead
       </div>
       <div className="col-sm-12 col-md-2">
