451352917f85e7610238587e0f635bd40ced5274
Kate Dubitski <kate.dubitski@gmail.com>
2018-08-20 18:31:37 -0400

    facebook pasport trial

--- client/components/FacebookLoginButton.js
+++ client/components/FacebookLoginButton.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import FacebookLogin from 'react-facebook-login'
 
-class FacebookLoginButton extends React.Component {
-  responseFacebook(response) {
-    console.log('facebook responce', response)
-  }
-  componentWillMount() {}
-  render() {
-    return (
-      <FacebookLogin
-        appId="237563910291590"
-        autoLoad={true}
-        fields="name,email,picture"
-        cssClass="btn btn-main"
-        callback={this.responseFacebook}
-      />
-    )
-  }
-}
-
-// const FacebookLoginButton = ()=> {
-//   return (
-//     <a href="/auth/facebook"><button type="button" className="btn btn-main">Login with Facebook</button></a>
-//   )
+// class FacebookLoginButton extends React.Component {
+//   responseFacebook(response) {
+//     console.log('facebook responce', response)
+//   }
+//   componentWillMount() {}
+//   render() {
+//     return (
+//       <FacebookLogin
+//         appId="237563910291590"
+//         autoLoad={true}
+//         fields="name,email,picture"
+//         cssClass="btn btn-main"
+//         callback={this.responseFacebook}
+//       />
+//     )
+//   }
 // }
+
+const FacebookLoginButton = () => {
+  return (
+    <a href="/auth/facebook">
+      <button type="button" className="btn btn-main">
+        Login with Facebook
+      </button>
+    </a>
+  )
+}
 
 export default FacebookLoginButton
