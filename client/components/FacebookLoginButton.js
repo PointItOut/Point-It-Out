52ab5027bb89379a7b368c531c3af0e1d2a5cb22
Kate Dubitski <kate.dubitski@gmail.com>
2018-08-20 17:49:45 -0400

    facebook login modifications

--- client/components/FacebookLoginButton.js
+++ client/components/FacebookLoginButton.js
@@ -1,22 +1,13 @@
 import React from 'react'
-import FacebookLogin from 'react-facebook-login'
 
-class FacebookLoginButton extends React.Component {
-  responseFacebook(response) {
-    console.log(response)
-  }
-
-  render() {
-    return (
-      <FacebookLogin
-        appId="237563910291590"
-        autoLoad={true}
-        fields="name,email,picture"
-        cssClass="btn btn-main"
-        callback={this.responseFacebook}
-      />
-    )
-  }
+const FacebookLoginButton = () => {
+  return (
+    <a href="/auth/facebook">
+      <button type="button" className="btn btn-main">
+        Login with Facebook
+      </button>
+    </a>
+  )
 }
 
 export default FacebookLoginButton
