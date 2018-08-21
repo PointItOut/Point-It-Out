a76d81622aac92c3ac15e8c62d55a360af513137
Kate Dubitski <kate.dubitski@gmail.com>
2018-08-21 16:16:58 -0400

    facebook removing friends from profile

--- server/auth/facebook.js
+++ server/auth/facebook.js
@@ -29,10 +29,7 @@ passport.use(
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
 //     /auth/facebook/callback
-router.get(
-  '/',
-  passport.authenticate('facebook', {scope: ['user_friends', 'email']})
-)
+router.get('/', passport.authenticate('facebook', {scope: 'email'}))
 
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
