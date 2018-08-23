26830b441344980d26e86b411904522dac6cc095
Alexandra Ash <alexandraleah@gmail.com>
2018-08-23 10:21:24 -0400

    adding path

--- client/components/Crown.js
+++ client/components/Crown.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react'
-import { Image } from 'react-konva'
+import React, {Component} from 'react'
+import {Image} from 'react-konva'
 import Konva from 'konva'
+import Path from 'path'
 
-const Crown = function (props) {
-    const { facecoords, pathname } = props
-    let image
-    if (facecoords) {
-        image = document.createElement("img")
-        image.src = pathname.includes('solo') ? 'crown.png' : '../crown.png'
-    }
+const Crown = function(props) {
+  const {facecoords, pathname} = props
+  let image
+  if (facecoords) {
+    image = document.createElement('img')
+    image.src = Path.join(__dirname, '/crown.png')
+  }
 
-
-    return (
-        <Image image={image} x={facecoords.x} y={facecoords.y - 100} width={facecoords.headWidth} height={100} />
-
-    )
+  return (
+    <Image
+      image={image}
+      x={facecoords.x}
+      y={facecoords.y - 100}
+      width={facecoords.headWidth}
+      height={100}
+    />
+  )
 }
 
 export default Crown
