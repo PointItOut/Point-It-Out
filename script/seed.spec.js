1ed5b9a0ebf90349f4ae7a1d2aba81ef62d73cf4
Alexandra Ash <alexandraleah@gmail.com>
2018-08-30 23:28:20 -0400

    testing(seed): forces test database to sync after each test seed so that will be empty for furture tests

--- script/seed.spec.js
+++ script/seed.spec.js
@@ -2,7 +2,12 @@
 /* global describe beforeEach it */
 
 const seed = require('./seed')
+const db = require('../server/db')
 
 describe('seed script', () => {
   it('completes successfully', seed)
+})
+
+afterEach(() => {
+  return db.sync({force: true})
 })
